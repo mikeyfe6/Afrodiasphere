03d4b789f7c9b2053afd6036f5d07a2ac85b77a8
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2021-06-20 18:56:21 +0200

    Update index.js

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -2,13 +2,39 @@ const { ApolloServer, gql } = require('apollo-server-lambda');
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    todos: [Todo]!
+  }
+  type Todo {
+    id: ID!
+    text: String!
+    done: Boolean!
+  }
+  type Mutation {
+    addTodo(text: String!): Todo
+    updateTodoDone(id: ID!): Todo
   }
 `;
 
+const todos = {};
+const todoIndex = 0;
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello World',
+    todos: () => {
+      return Object.values(todos);
+    },
+  },
+  Mutation: {
+    addTodo: (_, { text }) => {
+      todoIndex++;
+      const id = `key-${todoIndex}`;
+      todos[id] = { id, text, done: false };
+      return todos[id];
+    },
+    updateTodoDone: (_, { id }) => {
+      todos[id].done = true;
+      return todos[id];
+    },
   },
 };
 
