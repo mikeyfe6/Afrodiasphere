cd2518302528d696a78fa8132b04756cc52d8789
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2021-06-22 05:15:03 +0200

    testing faunadb

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,4 +1,9 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
+const faunadb = require('faunadb');
+
+const q = faunadb.query;
+
+const client = new faunadb.Client({ secret: process.env.GATSBY_FAUNADB });
 
 const typeDefs = gql`
   type Query {
@@ -15,28 +20,52 @@ const typeDefs = gql`
   }
 `;
 
-const todos = {};
-let todoIndex = 0;
-
 const resolvers = {
   Query: {
-    todos: (parents, args, { user }) => {
+    todos: async (parents, args, { user }) => {
       if (!user) {
         return [];
       }
-      return Object.values(todos);
+      const results = await client.query(
+        q.Paginate(q.Match(q.Index('todos_by_user'), user)),
+      );
+      return results.data.map(([ref, text, done]) => ({
+        id: ref.id,
+        text,
+        done,
+      }));
+      // return Object.values(todos);
     },
   },
+
   Mutation: {
-    addTodo: (_, { text }) => {
-      todoIndex += 1;
-      const id = `key-${todoIndex}`;
-      todos[id] = { id, text, done: false };
-      return todos[id];
+    addTodo: async (_, { text }, { user }) => {
+      if (!user) {
+        throw new Error('voer ieks goeds in man');
+      }
+      const results = await client.query(
+        q.Create(q.Collection('todos'), {
+          data: { text, done: false, owner: user },
+        }),
+      );
+
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
     },
-    updateTodoDone: (_, { id }) => {
-      todos[id].done = true;
-      return todos[id];
+
+    updateTodoDone: async (_, { id }, { user }) => {
+      if (!user) {
+        throw new Error('voer ieks goeds in man ali boalowii');
+      }
+      const results = await client.query(
+        q.Update(q.Ref(q.Collection('todos'), id), { data: { done: true } }),
+      );
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
     },
   },
 };
