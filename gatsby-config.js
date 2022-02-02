b28ccf57802724c3700e3316ce846cc6c9d6119d
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2022-02-03 00:29:19 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,7 @@ module.exports = {
         failOnError: true,
         // deprecated options and their defaults:
         base64Width: 20,
-        forceBase64Format: ``, // valid formats: png,jpg,webp
+        forceBase64Format: `webp`, // valid formats: png,jpg,webp
         useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
         stripMetadata: true,
         defaultQuality: 50,
